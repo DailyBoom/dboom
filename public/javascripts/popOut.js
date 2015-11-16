f9264dc6fcd3c4e85c2265f431be6039a6a68376
CptMaumau <etienne.maurin@epitech.eu>
2015-11-16 14:58:49 +0900

    kakao and email share

--- public/javascripts/popOut.js
+++ public/javascripts/popOut.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	Kakao.init('afdccb328111d08927408cad1957135c');
 	$("#facebook").on("click", function(){
 		var winTop = (screen.height / 2) - (350 / 2);
 		var winLeft = (screen.width / 2) - (600 / 2);	
@@ -33,5 +34,20 @@ $(document).ready(function() {
 		var winTop = (screen.height / 2) - (350 / 2);
 		var winLeft = (screen.width / 2) - (600 / 2);	
 		window.open("https://www.facebook.com/dialog/share?app_id=636096523200038&display=popup&href="+location.href+"&redirect_uri="+location.href, 'facebook', 'top=' + winTop + ',left=' + winLeft +',toolbar=0,status=1,width=600,height=350');
+	});
+	
+	$("#sharekakao").click(function(){
+		Kakao.Link.sendTalkLink({
+			label: "데일리 붐: 오늘 뭐 사지?",
+			image: {
+				src: "http://dailyboom.co/images/meta.jpg",
+				width: 1200,
+				height: 600
+			},
+			webButton: {
+				text: '글 보기',
+				url: "http://dailyboom.co"
+			}
+		});
 	});
 });
