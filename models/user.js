10c21cfd1a4da7059977dfab0ae42a90c3744b0c
CptMaumau <etienne.maurin@epitech.eu>
2015-12-10 18:03:29 +0900

    fix for fb signup

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ var SALT_WORK_FACTOR = 10;
 // create a schema
 var userSchema = new Schema({
   name: String,
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String },
   admin: Boolean,
