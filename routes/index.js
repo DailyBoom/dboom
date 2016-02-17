6316d8d07811cd346bfbd0b4e29ec9221aaa05af
CptMaumau <etienne.maurin@epitech.eu>
2016-02-17 18:56:29 +0900

    update on mall

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/mall', function(req, res, next) {
-  Product.find({ extend: 4 }, function(err, products) {
+  Product.find({ extend: 4 }, {}, { sort: { 'created_at' : -1 }}, function(err, products) {
     Product.find({ extend: 4, is_hot: true }, function(err, hotProducts) {
       res.render('mall', { title: "데일리 붐 쇼핑 몰", description: "데일리 붐은 ‘매일 폭탄 가격’이라는 뜻으로, 매일 한 가지의 상품을 한정된 시간 내에만 특가로 판매하는 웹사이트입니다.", products: products, hotProducts: hotProducts });
     });
@@ -81,7 +81,7 @@ router.get('/mall/:merchant', function(req, res, next) {
       console.log(err);
     if (!merchant)
       return res.redirect('/mall');
-    Product.find({ merchant_id: merchant._id, extend: 4 }, function(err, products) {
+    Product.find({ merchant_id: merchant._id, extend: 4 }, {}, { sort: { 'created_at' : -1 }}, function(err, products) {
       if (err)
         console.log(err);
       if (!products || products.length == 0)
