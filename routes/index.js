69112fff7512675931c478e8236023de40106b53
CptMaumau <etienne.maurin@epitech.eu>
2016-08-01 14:09:40 +0900

    yppuna transition: fixing text

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ router.get('/', function(req, res, next) {
           });
           var progress = (product.quantity - current_quantity) / product.quantity * 100;
           var sale = (product.old_price - product.price) / product.old_price * 100;
-          res.render('index', { progress: progress.toFixed(0), sale: sale.toFixed(0), product: product, pastProducts: pastProducts, title: "오늘 뭐 사지?" });
+          res.render('index', { progress: progress.toFixed(0), sale: sale.toFixed(0), product: product, pastProducts: pastProducts });
         });
       }
       else {
