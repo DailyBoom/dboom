26784210c095181b7986448e5047566bd56bd27d
CptMaumau <etienne.maurin@epitech.eu>
2016-10-19 13:13:56 +0900

    deploying new design to heroku

--- models/comment.js
+++ models/comment.js
@@ -5,6 +5,7 @@ var commentSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'User' },
 	order : { type: Schema.Types.ObjectId, ref: 'Order' },
 	article : { type: Schema.Types.ObjectId, ref: 'Article' },
+	product: { type: Schema.Types.ObjectId, ref: 'Product' },
 	type: Number,
 	name: String,
 	body: String,
