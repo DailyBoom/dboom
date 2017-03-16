6c88cd78ac12080a763d3e7e7130a85f366aa5dc
CptMaumau <etienne.maurin@epitech.eu>
2017-03-16 14:20:48 +0900

    update for new homepage

--- routes/index.js
+++ routes/index.js
@@ -248,17 +248,21 @@ router.get('/home', function(req, res, next) {
     });
     behavior.save();
   }
-  Product.find({ boxZone: req.session.zone, scheduled_at: moment().date(1).hour(0).minute(0).second(0).millisecond(0) }, {}, {sort : { 'scheduled_at' : 1 }}).populate('boxProducts').exec(function (err, products) {
-    console.log(products);
+  Product.find({ boxZone: req.session.zone, scheduled_at: moment().date(1).hour(config.Timezone).minute(0).second(0).millisecond(0) }, {}, {sort : { 'scheduled_at' : 1 }}).populate('boxProducts').exec(function (err, products) {
+    // console.log(products);
     var query = Product.find({ extend: 4, is_published: true, is_hot: true });
-    //query.where('product_region.'+req.session.zone, true);
+    // query.where('product_region.'+req.session.zone, true);
     query.limit(4).sort({ 'created_at' : -1 }).exec(function (err, hotProducts) {
-      Product.find({ extend: 4, is_published: true, $or: [ { created_at: { $gte: moment().subtract(2, 'weeks') } }, { is_new: true } ] }).where('product_region.'+req.session.zone, true).exec(function(err, newProducts) {
-        Comment.find( { product: products[0].id }).populate('user').exec(function(err, comments) {
-          res.render('beta', { progress: 75, products: products, hotProducts: hotProducts, newProducts: newProducts, comments: comments });
+    //Product.find({ extend: 4, is_published: true, $or: [ { created_at: { $gte: moment().subtract(2, 'weeks') } }, { is_new: true } ] }).where('product_region.'+req.session.zone, true).exec(function(err, newProducts) {
+    //     Comment.find( { product: products[0].id }).populate('user').exec(function(err, comments) {
+      Article.find({ published: true, video: {$in: [null, false]} }, {}, { sort: { 'created_at': -1 } }).limit(3).exec(function(err, articles) {
+        Article.find({ published: true, video: true }, {}, { sort: { 'created_at': -1 } }).limit(3).exec(function(err, videos) {
+          res.render('index', { products: products, articles: articles, videos: videos, hotProducts: hotProducts });
         });
       });
     });
+    //   });
+    // });
   });
 });
 
