4376108113837656e725451dcf8804cbd75a200e
CptMaumau <etienne.maurin@epitech.eu>
2017-04-10 17:48:08 +0900

    finished reviews upload and display on homepage

--- models/homepage.js
+++ models/homepage.js
@@ -5,15 +5,10 @@ var homepageSchema = new Schema({
     main_banner: String,
     right_1_banner: String,
     right_2_banner: String,
-    beauty_banner: String,
-    news_banner: String,
-    pedia_banner: String,
     deal_banner: String,
     fav_banner: String,
     organic_banner: String,
-    review_1_banner: String,
-    review_2_banner: String,
-    review_3_banner: String
+    reviews: [{ banner: String, name: String, avatar: String, text: String}]
 });
 
 var Homepage = mongoose.model("Homepage", homepageSchema);
--- routes/homepage.js
+++ routes/homepage.js
@@ -40,7 +40,7 @@ router.get('/homepage/edit', isAdmin, function(req, res) {
     res.render('homepage/edit');
 });
 
-router.post('/homepage/edit', isAdmin, upload.fields([{ name: "main_banner" }, { name: "right_1_banner" }, { name: "right_2_banner" }, { name: "video_banner" }, { name: "beauty_banner" }, { name: "news_banner" }, { name: "pedia_banner" }, { name: "deal_banner" }, { name: "fav_banner" }, { name: "organic_banner" }, { name: "review_1_banner" }, { name: "review_2_banner" }, { name: "review_3_banner" }]), function(req, res) {
+router.post('/homepage/edit', isAdmin, upload.fields([{ name: "main_banner" }, { name: "right_1_banner" }, { name: "right_2_banner" }, { name: "deal_banner" }, { name: "fav_banner" }, { name: "organic_banner" }, { name: "review_1_banner" }, { name: "review_2_banner" }, { name: "review_3_banner" }, { name: "review_1_avatar" }, { name: "review_2_avatar" }, { name: "review_3_avatar" }]), function(req, res) {
     Homepage.findOne({}, function(err, homepage) {
         if (req.files['main_banner'])
             homepage.main_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['main_banner'][0].key
@@ -48,26 +48,29 @@ router.post('/homepage/edit', isAdmin, upload.fields([{ name: "main_banner" }, {
             homepage.right_1_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['right_1_banner'][0].key
         if (req.files['right_2_banner'])
             homepage.right_2_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['right_2_banner'][0].key
-        if (req.files['video_banner'])
-            homepage.video_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['video_banner'][0].key
-        if (req.files['beauty_banner'])
-            homepage.beauty_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['beauty_banner'][0].key
-        if (req.files['news_banner'])
-            homepage.news_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['news_banner'][0].key
-        if (req.files['pedia_banner'])
-            homepage.pedia_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['pedia_banner'][0].key
         if (req.files['deal_banner'])
             homepage.deal_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['deal_banner'][0].key
         if (req.files['fav_banner'])
             homepage.fav_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['fav_banner'][0].key
         if (req.files['organic_banner'])
             homepage.organic_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['organic_banner'][0].key
+
+        console.log(req.body);
+        homepage.reviews = req.body.reviews;
+
         if (req.files['review_1_banner'])
-            homepage.review_1_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_1_banner'][0].key
+            homepage.reviews[0].banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_1_banner'][0].key
         if (req.files['review_2_banner'])
-            homepage.review_2_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_2_banner'][0].key
+            homepage.reviews[1].banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_2_banner'][0].key
         if (req.files['review_3_banner'])
-            homepage.review_3_banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_3_banner'][0].key
+            homepage.reviews[2].banner = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_3_banner'][0].key
+        if (req.files['review_1_avatar'])
+            homepage.reviews[0].avatar = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_1_avatar'][0].key
+        if (req.files['review_2_avatar'])
+            homepage.reviews[1].avatar = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_2_avatar'][0].key
+        if (req.files['review_3_avatar'])
+            homepage.reviews[2].avatar = "https://s3.ap-northeast-2.amazonaws.com/dailyboom/" + req.files['review_3_avatar'][0].key
+
 
         homepage.save(function(err) {
             if (err)
